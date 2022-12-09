3067b98bfa6bfcf5335685748656a8e6579ad0f3
Lilia Vasileva <lilia.vvasileva@gmail.com>
2022-12-09 06:50:50 +0200

    Adding WordWeCounter class

---
+++ word_counter/word_counter.py
@@ -0,0 +1,31 @@
+import requests
+import html5lib
+import re
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+
+class WordWebCounter():
+    def __init__(self, word, url):
+        self.word = word
+        self.url = url
+
+    @property
+    def word(self):
+        return self.word
+
+    @word.setter
+    def word(self, value):
+        if value == "":
+            raise ValueError("Word cannot be a empty value")
+        self.__word = value
+
+    @property
+    def url(self):
+        return self.url
+
+    @url.setter
+    def url(self, value):
+        if value == "":
+            raise ValueError("URL cannot be a empty value")
+        self.__url = value

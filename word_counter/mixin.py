17567c703c3cac81ee51a345e1caeaf0a98ad968
Lilia Vasileva <lilia.vvasileva@gmail.com>
2022-12-11 11:52:38 +0200

    Adding documentation for the program

--- word_counter/mixin.py
+++ word_counter/mixin.py
@@ -4,6 +4,7 @@ from urllib.request import urlopen
 
 
 class CreateSoupMixin(BeautifulSoup):
+    """ create_soup() function is creating a BeautifulSoup object which represents the document as a nested data structure. """
     def create_soup(self, url):
         html = urlopen(url).read()
         soup = BeautifulSoup(html, features="html.parser")
@@ -11,6 +12,10 @@ class CreateSoupMixin(BeautifulSoup):
 
 
 class HtmlToTextMixin():
+    """
+    html_to_text() function accept soup object and clear from the soup object all scripts and styles tags.
+    Return text representation of the html.
+    """
     def html_to_text(self, soup):
         for script in soup(["script", "style"]):
             script.extract()
@@ -18,6 +23,10 @@ class HtmlToTextMixin():
 
 
 class ClearTextMixin():
+    """
+    clear_text() function accept string and clears all empty spaces and empty lines.
+    Returns str: clean text.
+    """
     def clear_text(self, text):
         lines = [line.strip() for line in text.splitlines()]
         clear_text = [phrase.strip() for line in lines for phrase in line.split(" ") if phrase]
@@ -25,6 +34,10 @@ class ClearTextMixin():
 
 
 class CountWordsMixin():
+    """
+    count_words() function accept word and text, count how many times the word occure in the text, total words in the text
+    Returns  int: count_word and count_all_word.
+    """
     def count_words(self, word, text):
         count_word = 0
         count_all_word = 0
--- word_counter/word_counter.py
+++ word_counter/word_counter.py
@@ -2,9 +2,16 @@ from mixin import CreateSoupMixin, ClearTextMixin, CountWordsMixin, HtmlToTextMi
 
 
 class WordWebCounter(CreateSoupMixin, HtmlToTextMixin, ClearTextMixin, CountWordsMixin):
+    """
+    On initialization of class WordWebCounter receives word and url.
+    The program checks if the word and the url are valid data.
+    If they are empty str the program will raise an error with appropriate error massage.
+    Please check docs for report() function and __str__ magic method.
+    """
     def __init__(self, word, url):
         self.word = word
         self.url = url
+
 
     @property
     def word(self):
@@ -28,6 +35,13 @@ class WordWebCounter(CreateSoupMixin, HtmlToTextMixin, ClearTextMixin, CountWord
 
 
     def report(self):
+        """
+        report() function create a soup object, then extraxt the text representation of the html,
+        clear the text from empty lines and spaces.
+        Please check documentation of CreateSoupMixin, HtmlToTextMixin, ClearTextMixin, CountWordsMixin.
+        Return message with how many times the given word occures on the url and counts all words.
+
+        """
         soup = self.create_soup(self.__url)
         text = self.html_to_text(soup)
         clean_text = self.clear_text(text)
@@ -41,6 +55,10 @@ class WordWebCounter(CreateSoupMixin, HtmlToTextMixin, ClearTextMixin, CountWord
 
 
     def __str__(self):
+        """
+        Returns message with the main functionality of the class personalized for the current object.
+
+        """
         return f'How many words and how many times the word - "{self.__word}" occur on the "{self.__url}".'
 
 
@@ -48,3 +66,4 @@ class WordWebCounter(CreateSoupMixin, HtmlToTextMixin, ClearTextMixin, CountWord
 test = WordWebCounter('uniberg', 'https://uniberg.com/en/')
 print(test)
 print(test.report())
+print(test.report.__doc__)
